f105dcdaaf6b4987d93df942c1bb3865ede71ca1
Adam J Norris <77121869+ajn-riva@users.noreply.github.com>
2021-12-13 20:55:09 -0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 					 
 					 
 driver = webdriver.Remote(
-   command_executor="https://selenium.mgmt.odos-tc.demoriva.com/wd/hub",
+   command_executor="http://selenium3-selenium-hub.jenkins.svc.cluster.local:4444/wd/hub",
    desired_capabilities={
             "browserName": "chrome",
             "browserVersion": "latest",
